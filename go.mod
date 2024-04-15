c256d5e13ee62aba0364f501bd8b6f60bd0ccd1d
jonah-butler <jonahbutler6@gmail.com>
2024-04-15 18:09:56 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module image-compressor
+
+go 1.21.0
+
+require (
+	github.com/aws/aws-lambda-go v1.46.0 // indirect
+	github.com/h2non/bimg v1.1.9 // indirect
+)
---
+++ main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"mime"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/h2non/bimg"
+)
+
+const TEMP_UPLOADS_DIR = "temp_uploads"
+
+// typing ease helper
+func errorHandler(err error) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{StatusCode: 400}, err
+}
+
+// simplifies getting content-type or other header data
+func convertToStandardHeader(header map[string]string) http.Header {
+	h := http.Header{}
+	for k, v := range header {
+		h.Add(strings.TrimSpace(k), v)
+	}
+	return h
+}
+
+func decodeRequestBody(request events.APIGatewayProxyRequest, contentType string) (*multipart.Reader, error) {
+	mediatype, params, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		return nil, err
+	}
+
+	// ensure mediaType is multipart
+	if strings.Index(strings.ToLower(strings.TrimSpace(mediatype)), "multipart/") != 0 {
+		return nil, errors.New("content type invalid")
+	}
+
+	paramKeys := convertToStandardHeader(params)
+
+	boundary := paramKeys.Get("boundary")
+	if len(boundary) == 0 {
+		return nil, err
+	}
+
+	if request.IsBase64Encoded {
+		decoded, err := base64.StdEncoding.DecodeString(request.Body)
+		if err != nil {
+			return nil, err
+		}
+		return multipart.NewReader(bytes.NewReader(decoded), boundary), nil
+	}
+	return multipart.NewReader(strings.NewReader(request.Body), boundary), nil
+
+}
+
+type LambdaPayload struct {
+	UploadPath string
+	UploadTags string
+	Upload     []byte
+}
+
+func extractFormData(reader *multipart.Reader) (*LambdaPayload, error) {
+	lp := LambdaPayload{}
+
+	for {
+		part, err := reader.NextPart()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		if part.FileName() != "" && part.FormName() == "upload" {
+
+			file, err := io.ReadAll(part)
+			if err != nil {
+				return nil, err
+			}
+			lp.Upload = file
+
+		} else {
+
+			content, err := io.ReadAll(part)
+			if err != nil {
+				return nil, err
+			}
+
+			if part.FormName() == "upload-path" {
+				lp.UploadPath = string(content)
+			} else if part.FormName() == "upload-tags" {
+				lp.UploadTags = string(content)
+			}
+
+		}
+	}
+
+	return &lp, nil
+}
+
+func createFolder(dirname string) error {
+	_, err := os.Stat(dirname)
+	if os.IsNotExist(err) {
+		errDir := os.MkdirAll(dirname, 0755)
+		if errDir != nil {
+			return errDir
+		}
+	}
+	return nil
+}
+
+func compressImage(buffer []byte, filename string) error {
+	processed, err := bimg.NewImage(buffer).Process(bimg.Options{
+		StripMetadata: true,
+		Palette:       true,
+		// Speed: 9, / /may not need this...
+	})
+	if err != nil {
+		return err
+	}
+
+	writeError := bimg.Write(fmt.Sprintf("./"+TEMP_UPLOADS_DIR+"/%s", filename), processed)
+	if writeError != nil {
+		return writeError
+	}
+
+	return nil
+}
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	headers := convertToStandardHeader(request.Headers)
+
+	contentType := headers.Get("content-type")
+
+	reader, err := decodeRequestBody(request, contentType)
+	if err != nil {
+		return errorHandler(err)
+	}
+
+	payload, err := extractFormData(reader)
+	if err != nil {
+		return errorHandler(err)
+	}
+	log.Println("payload: ", payload)
+
+	errDir := createFolder("test_uploads")
+	if errDir != nil {
+		panic(errDir)
+	}
+
+	err = compressImage(payload.Upload, "test1.png")
+	if err != nil {
+		return errorHandler(err)
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "hmmmmmmmm",
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
